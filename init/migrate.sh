d977894b10f5db0cca98223e6060cafba0834cab
Gary Bezruchko <gary.bezruchko@gmail.com>
2024-12-04 22:00:05 +0100

    Running example

--- init/migrate.sh
+++ init/migrate.sh
@@ -4,7 +4,8 @@ echo "Granting Bitrix user privileges..."
 
 mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "
 GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
-GRANT SUPER ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+GRANT SUPER ON *.* TO '${MYSQL_USER}'@'%';
+GRANT SESSION_VARIABLES_ADMIN ON *.* TO '${MYSQL_USER}'@'%';
 FLUSH PRIVILEGES;
 ";
 
--- php/startup/sshd.sh
+++
@@ -1,3 +0,0 @@
-if [ "$SET_UP_SSH_SERVER" = 'true' ]; then
-  usr/sbin/sshd -D
-fi
